d42bf9b99c2d16be57bf85eed20c991866ebe731
chdonncha <chdonncha@gmail.com>
2016-11-10 23:26:00 +0000

    updates

--- Examples/SocketProgramming/SocClient.java
+++ Examples/SocketProgramming/SocClient.java
@@ -3,7 +3,15 @@ import java.new.Socket;
 public class SocClient {
 
     public static void main(String[] args) {
-        String ip = "";
-        
+        String ip = "localhost";
+        init port = 9999; // 0-1023 to 65535
+        Socket s = new Socket(ip,port);
+
+        String str = "Donncha Cassidy";
+
+        OutputStreamWriter os = new OutputStreamWriter(s.getOutputStream());
+        PrintWriter out = new PrintWriter(os);
+         
+
     }
 }
