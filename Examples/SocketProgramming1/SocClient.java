5656545220eeb403fdf1e6c9340eab78507e6466
chdonncha <chdonncha@gmail.com>
2016-11-11 01:13:05 +0000

    added error handling

--- Examples/SocketProgramming1/SocClient.java
+++ Examples/SocketProgramming1/SocClient.java
@@ -5,7 +5,7 @@ import java.net.Socket;
 public class SocClient 
 {
 
-    public static void main(String[] args) 
+    public static void main(String[] args) throws Exception
     {
         String ip = "localhost";
         int port = 9999; // 0-1023 to 65535
