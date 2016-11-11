36c5affd7127aad92648e037410d45454b11162d
chdonncha <chdonncha@gmail.com>
2016-11-11 01:14:00 +0000

    fixed error

--- Examples/SocketProgramming1/SocServer.java
+++ Examples/SocketProgramming1/SocServer.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class socServer 
+public class SocServer 
 {
     public static void main(String[] args) throws Exception
     {
