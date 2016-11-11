4f041a9f43bf5b3067c0e719f0b1637893011192
chdonncha <chdonncha@gmail.com>
2016-11-11 01:04:46 +0000

    server updates

---
+++ Examples/SocketProgramming/SocServer.java
@@ -0,0 +1,18 @@
+public class socServer 
+{
+    public static void main(String[] args) thorows Exception
+    {
+        System.out.println("Server is started");
+        ServerSocket ss = new ServerSocket(9999);
+
+        System.out.println("Server is waiting for client request");
+        Socket s = ss.accept();
+
+        System.out.println("Client Accepted");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String str = br.readLine();
+
+        System.out.println("Client Data : " + str);
+    }    
+}
