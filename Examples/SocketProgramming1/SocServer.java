39721f04a2adb23720ed107ba7850e52b653ec8d
chdonncha <chdonncha@gmail.com>
2016-11-11 01:09:08 +0000

    added more imports

--- Examples/SocketProgramming1/SocServer.java
+++ Examples/SocketProgramming1/SocServer.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class socServer 
 {
     public static void main(String[] args) thorows Exception
