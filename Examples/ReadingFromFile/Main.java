a0ad51053b1df6f44cf6df7941e32cbed340c433
chdonncha <chdonncha@gmail.com>
2016-11-13 23:04:26 +0000

    updates

--- Examples/ReadingFromFile/Main.java
+++ Examples/ReadingFromFile/Main.java
@@ -5,18 +5,18 @@ import java.nio.charset.Charset;
 public class Main
 {  
    public static void main(String[] args) {
-         String line;
-
-        try (
-            InputStream fis = new FileInputStream("beans.txt");
-            InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
-            BufferedReader br = new BufferedReader(isr);
-            //System.out.println(line);
-        ).catch (IOException e) {
-            System.err.println("error: " + e.getMessage());
-        }
-            while ((line = br.readLine()) !=null) {
-                String[] words = line.split(" ");        
+       
+        try {
+            File file = new File("beans.txt");       
+            FileReader filereader = new FileReader(file);
+            BufferedReader br = new BufferedReader(filereader);
+            String line;
+            while((line = br.readLine()) != null) {
+                String[] words = line.split(" ");
+                System.out.println(line);
+            }
+        }catch(Exception e){
+            System.out.println("Exception: " + e);
         }
     }  
 }
