935dc9176db8c092784563ea320819cf5fb62c3c
chdonncha <chdonncha@gmail.com>
2016-11-14 12:29:02 +0000

    auto formatting

--- Assignment/AuctionServer.java
+++ Assignment/AuctionServer.java
@@ -3,82 +3,75 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class AuctionServer implements Runnable
-{
+public class AuctionServer implements Runnable {
 
-   // Array of clients
-   private AuctionServerThread clients[] = new AuctionServerThread[50];
-   private ServerSocket server = null;
-   private Thread       thread = null;
-   private int clientCount = 0;
+  // Array of clients
+  private AuctionServerThread clients[] = new AuctionServerThread[50];
+  private ServerSocket server = null;
+  private Thread thread = null;
+  private int clientCount = 0;
 
-   public String item = "chair";
+  public String item = "chair";
 
-   public AuctionServer(int port)
-   {
-	  try {
+  public AuctionServer(int port) {
+    try {
 
-		 System.out.println("Binding to port " + port + ", please wait  ...");
-       server = new ServerSocket(port);
-         System.out.println("Server started: " + server.getInetAddress());
-         start();
-      }
-      catch(IOException ioe)
-      {
-		  System.out.println("Can not bind to port " + port + ": " + ioe.getMessage());
-      }
-   }
-
-   public void run()
-   {
-	  while (thread != null)
-      {
-		 try{
-
-			System.out.println("Waiting for a client ...");
-            addThread(server.accept());
-            displayItem();
-			int pause = (int)(Math.random()*3000);
-			Thread.sleep(pause);
-
-         }
-         catch(IOException ioe){
-			System.out.println("Server accept error: " + ioe);
-			stop();
-         }
-         catch (InterruptedException e){
-		 	System.out.println(e);
-		 }
+      System.out.println("Binding to port " + port + ", please wait  ...");
+      server = new ServerSocket(port);
+      System.out.println("Server started: " + server.getInetAddress());
+      start();
+    } catch (IOException ioe) {
+      System.out.println("Can not bind to port " + port + ": " +
+                         ioe.getMessage());
     }
-   }
+  }
+
+  public void run() {
+    while (thread != null) {
+      try {
+
+        System.out.println("Waiting for a client ...");
+        addThread(server.accept());
+        displayItem();
+        int pause = (int)(Math.random() * 3000);
+        Thread.sleep(pause);
+
+      } catch (IOException ioe) {
+        System.out.println("Server accept error: " + ioe);
+        stop();
+      } catch (InterruptedException e) {
+        System.out.println(e);
+      }
+    }
+  }
 
   public ArrayList<String> readFromFile() {
-      String line = "test";
-      String[] words = {"test"};
-      ArrayList<String> list = new ArrayList<String>();
+    String line = "test";
+    String[] words = {"test"};
+    ArrayList<String> list = new ArrayList<String>();
 
-        try {
-            File file = new File("items.txt");       
-            FileReader filereader = new FileReader(file);
-            BufferedReader br = new BufferedReader(filereader);
+    try {
+      File file = new File("items.txt");
+      FileReader filereader = new FileReader(file);
+      BufferedReader br = new BufferedReader(filereader);
 
-            while((line = br.readLine()) != null) {
-                words = line.split(" ");
-                list.addAll( Arrays.asList(line));
-            }
-          
-            // for (String s : list) {
-            //   System.out.println(s);
-            // }
+      while ((line = br.readLine()) != null) {
+        words = line.split(" ");
+        list.addAll(Arrays.asList(line));
+      }
 
-        }catch(Exception e){
-            System.out.println("Exception: " + e);
-        }  
-      return list;
-   }
+      // for (String s : list) {
+      //   System.out.println(s);
+      // }
+
+    } catch (Exception e) {
+      System.out.println("Exception: " + e);
+    }
+    return list;
+  }
 
   public void displayItem() {
-    //String[] item = readFromFile();
+    // String[] item = readFromFile();
     readFromFile();
 
     for (String s : list) {
@@ -86,7 +79,7 @@ public class AuctionServer implements Runnable
     }
 
     try {
-      while(true) {
+      while (true) {
         broadcast("item for auction: ");
         Thread.sleep(5 * 1000);
       }
@@ -95,100 +88,87 @@ public class AuctionServer implements Runnable
     }
   }
 
-  public void start()
-    {
-		if (thread == null) {
-		  thread = new Thread(this);
-          thread.start();
-       }
+  public void start() {
+    if (thread == null) {
+      thread = new Thread(this);
+      thread.start();
     }
+  }
 
-   public void stop(){
-	   thread = null;
+  public void stop() { thread = null; }
 
-   }
+  private int findClient(int ID) {
+    for (int i = 0; i < clientCount; i++)
+      if (clients[i].getID() == ID)
+        return i;
+    return -1;
+  }
 
-   private int findClient(int ID)
-   {
-	   for (int i = 0; i < clientCount; i++)
-         if (clients[i].getID() == ID)
-            return i;
-      return -1;
-   }
+  // take the user input and send to all users
+  public synchronized void broadcast(String input) {
 
-   // take the user input and send to all users
-   public synchronized void broadcast(String input)
-   {
+    // printout input sent from client
+    // System.out.println("C: " + input);
 
-     // printout input sent from client
-     //System.out.println("C: " + input);
-
-	  //  if (input.equals(".bye")){
-		//   clients[findClient(ID)].send(".bye");
+    //  if (input.equals(".bye")){
+    //   clients[findClient(ID)].send(".bye");
     //       remove(ID);
     //    }
     //    else
-         for (int i = 0; i < clientCount; i++){
+    for (int i = 0; i < clientCount; i++) {
       // broadcast to everyone but the user it was sent from
-			//if(clients[i].getID() != ID)
-            	clients[i].send(input); // sends messages to clients
-		}
-       notifyAll();
-   }
-   public synchronized void remove(int ID)
-   {
-	  int pos = findClient(ID);
-      if (pos >= 0){
-		 AuctionServerThread toTerminate = clients[pos];
-         System.out.println("Removing client thread " + ID + " at " + pos);
+      // if(clients[i].getID() != ID)
+      clients[i].send(input); // sends messages to clients
+    }
+    notifyAll();
+  }
+  public synchronized void remove(int ID) {
+    int pos = findClient(ID);
+    if (pos >= 0) {
+      AuctionServerThread toTerminate = clients[pos];
+      System.out.println("Removing client thread " + ID + " at " + pos);
 
-         if (pos < clientCount-1)
-            for (int i = pos+1; i < clientCount; i++)
-               clients[i-1] = clients[i];
-         clientCount--;
+      if (pos < clientCount - 1)
+        for (int i = pos + 1; i < clientCount; i++)
+          clients[i - 1] = clients[i];
+      clientCount--;
 
-         try{
-			 toTerminate.close();
-	     }
-         catch(IOException ioe)
-         {
-			 System.out.println("Error closing thread: " + ioe);
-		 }
-		 toTerminate = null;
-		 System.out.println("Client " + pos + " removed");
-		 notifyAll();
+      try {
+        toTerminate.close();
+      } catch (IOException ioe) {
+        System.out.println("Error closing thread: " + ioe);
       }
-   }
+      toTerminate = null;
+      System.out.println("Client " + pos + " removed");
+      notifyAll();
+    }
+  }
 
-   private void addThread(Socket socket)
-   {
-	  if (clientCount < clients.length){
+  private void addThread(Socket socket) {
+    if (clientCount < clients.length) {
 
-		 System.out.println("Client accepted: " + socket);
-         //clients[clientCount] = new AuctionServerThread(this, socket);
-         AuctionServerThread client = new AuctionServerThread(this, socket);
-         try{
-			client.open();
-      broadcast("item for auction: chair");
-            client.start();
-            clients[clientCount] = client;
-            clientCount++;
-         }
-         catch(IOException ioe){
-			 System.out.println("Error opening thread: " + ioe);
-		  }
-	  }
-      else
-         System.out.println("Client refused: maximum " + clients.length + " reached.");
-   }
+      System.out.println("Client accepted: " + socket);
+      // clients[clientCount] = new AuctionServerThread(this, socket);
+      AuctionServerThread client = new AuctionServerThread(this, socket);
+      try {
+        client.open();
+        broadcast("item for auction: chair");
+        client.start();
+        clients[clientCount] = client;
+        clientCount++;
+      } catch (IOException ioe) {
+        System.out.println("Error opening thread: " + ioe);
+      }
+    } else
+      System.out.println("Client refused: maximum " + clients.length +
+                         " reached.");
+  }
 
-
-   public static void main(String args[]) {
-	   AuctionServer server = null;
-      if (args.length != 1)
-         System.out.println("Usage: java AuctionServer port");
-      else
-         server = new AuctionServer(Integer.parseInt(args[0]));
-   }
-
+  public static void main(String args[]) {
+    AuctionServer server = null;
+    if (args.length != 1)
+      System.out.println("Usage: java AuctionServer port");
+    else
+      server = new AuctionServer(Integer.parseInt(args[0]));
+  }
 }
--- Examples/ReadingFromFile/Main.java
+++ Examples/ReadingFromFile/Main.java
@@ -2,21 +2,20 @@ import java.net.*;
 import java.io.*;
 import java.nio.charset.Charset;
 
-public class Main
-{  
-   public static void main(String[] args) {
-       
-        try {
-            File file = new File("beans.txt");       
-            FileReader filereader = new FileReader(file);
-            BufferedReader br = new BufferedReader(filereader);
-            String line;
-            while((line = br.readLine()) != null) {
-                String[] words = line.split(" ");
-                System.out.println(line);
-            }
-        }catch(Exception e){
-            System.out.println("Exception: " + e);
-        }
-    }  
+public class Main {
+  public static void main(String[] args) {
+
+    try {
+      File file = new File("beans.txt");
+      FileReader filereader = new FileReader(file);
+      BufferedReader br = new BufferedReader(filereader);
+      String line;
+      while ((line = br.readLine()) != null) {
+        String[] words = line.split(" ");
+        System.out.println(line);
+      }
+    } catch (Exception e) {
+      System.out.println("Exception: " + e);
+    }
+  }
 }
